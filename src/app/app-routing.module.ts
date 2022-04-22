bbfd2b7ee4216c43f90f027f4489b0356e785b28
Gerson Junior <gersonjuniordev@gmail.com>
2022-04-22 13:00:27 -0300

    feat: adding projects and about me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/components';
 import { HomeComponent } from './pages/home/components/home/home.component';
 import { ProjectComponent } from './pages/project/components/project/project.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'projects', component: ProjectComponent},
-  {path: '', component: HomeComponent}
+  {path: 'about', component: AboutComponent}
+
+
 ];
 
 @NgModule({
